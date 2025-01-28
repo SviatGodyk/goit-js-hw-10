8a50f9304696e7c2fadbb4d9426792f2ef527c5e
Sviatoslav Godyk <godyk1992@gmail.com>
2025-01-28 18:01:26 +0800

    add hw-10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,23 +1,28 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 let userSelectedDate = null;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  elements: {
-    days: document.querySelector('.js-timer-days'),
-    hours: document.querySelector('.js-timer-hours'),
-    minutes: document.querySelector('.js-timer-minutes'),
-    seconds: document.querySelector('.js-timer-seconds'),
-  },
   onClose(selectedDates) {
     console.log(selectedDates[0]);
     if (selectedDates[0] < new Date()) {
-      return alert('Please choose a date in the future');
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
     } else {
       userSelectedDate = selectedDates[0];
+      iziToast.success({
+        title: 'OK',
+        message: 'Valid date selected',
+      });
       startBtn.disabled = false;
     }
   },
@@ -25,40 +30,49 @@ const options = {
 
 const imputEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button');
+const daysEl = document.querySelector('.js-timer-days');
+const hoursEl = document.querySelector('.js-timer-hours');
+const minutesEl = document.querySelector('.js-timer-minutes');
+const secondsEl = document.querySelector('.js-timer-seconds');
+
 startBtn.disabled = true;
 flatpickr(imputEl, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+
+  const hours = Math.floor((ms % day) / hour);
+
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const addLeadingZero = value => value.toString().padStart(2, '0');
 
 startBtn.addEventListener('click', () => {
   startBtn.disabled = true;
   imputEl.disabled = true;
   const intervalId = setInterval(() => {
     const diff = userSelectedDate - new Date();
-    const timeConvertMs = this.convertMs(diff);
-    this.elements.days.textContent = timeConvertMs.days;
-    this.elements.hours.textContent = timeConvertMs.hours;
-    this.elements.minutes.textContent = timeConvertMs.minutes;
-    this.elements.seconds.textContent = timeConvertMs.seconds;
+
+    if (diff <= 0) {
+      clearInterval(intervalId);
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(diff);
+
+    daysEl.textContent = addLeadingZero(days);
+    hoursEl.textContent = addLeadingZero(hours);
+    minutesEl.textContent = addLeadingZero(minutes);
+    secondsEl.textContent = addLeadingZero(seconds);
   }, 1000);
-}),
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-  };
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+});
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,37 @@
-console.log('Hi');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('.form');
+const inputEl = document.querySelector('.input-delay');
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const inputValue = event.target[0].value;
+  console.log(inputValue);
+
+  const stateEl = event.target[2].checked;
+  console.log(stateEl);
+
+  new Promise((res, rej) => {
+    setTimeout(() => {
+      if (stateEl) {
+        return res();
+      } else {
+        rej();
+      }
+    }, inputValue);
+  })
+    .then(response => {
+      return iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${inputValue}ms`,
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${inputValue}ms`,
+      });
+    });
+});
